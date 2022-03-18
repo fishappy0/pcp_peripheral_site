121961c680b077e74ca2ec812abfd3589bb68675
Fishappy0 <fishappy0@gmail.com>
2022-03-18 14:15:20 +0700

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import '../styles/globals.css'
+import '../styles/dist/global.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function pcp_peripherals({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default pcp_peripherals
